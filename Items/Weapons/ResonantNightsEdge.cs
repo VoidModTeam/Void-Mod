8a44b1f889278c80d20c356c7d9226addee5b1e5
Frederico Volpinide Oliveira <layeph@gmail.com>
2020-09-09 23:04:47 -0300

    Created NPCs Folder, Created Weapons Folder, Moved testSword, Created HellNPC, Created ResonanntNightsEdge

---
+++ Items/Weapons/ResonantNightsEdge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace VoidMod.Items
+{
+    public class RasonantNightsEdge : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Resonant Night's Edge")
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 16;
+            item.height = 16;
+            item.rare = ItemRarityID.LightRed;
+            item.value = Item.buyPrice(gold: 20);
+            item.maxStack = 1;
+        }
+    }
+}
